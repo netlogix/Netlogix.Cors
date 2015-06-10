cb7a41c971e6b28597ddece7f37dde24c7de3074
Sascha Nowak <sascha.nowak@netlogix.de>
2015-06-10 17:56:08 +0200

    [BUGFIX] Allow all restfull http methods

--- Classes/Netlogix/Cors/Http/Component/CorsComponent.php
+++ Classes/Netlogix/Cors/Http/Component/CorsComponent.php
@@ -2,13 +2,8 @@
 namespace Netlogix\Cors\Http\Component;
 
 /*                                                                        *
- * This script belongs to the TYPO3 Flow framework.                       *
+ * This script belongs to the TYPO3 Flow package "Netlogix.Cors".         *
  *                                                                        *
- * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU Lesser General Public License, either version 3   *
- * of the License, or (at your option) any later version.                 *
- *                                                                        *
- * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
 use TYPO3\Flow\Annotations as Flow;
@@ -39,7 +34,7 @@ class CorsComponent implements \TYPO3\Flow\Http\Component\ComponentInterface {
 	 * @return void
 	 */
 	public function handle(\TYPO3\Flow\Http\Component\ComponentContext $componentContext) {
-		static $possibleMethods = array('GET', 'POST', 'OPTIONS');
+		static $possibleMethods = array('GET', 'POST', 'OPTIONS', 'DELETE', 'PUT');
 
 		$corsService = new \Netlogix\Cors\Service\CorsService();
 
